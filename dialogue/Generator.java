e0d272bcf5f92ea702b748d50f70e519b02e2c80
pc <61981562+cep450@users.noreply.github.com>
2023-07-09 12:17:54 -0400

    removed extra space, deleted old folder

--- dialogue/Generator.java
+++ dialogue/Generator.java
@@ -76,7 +76,7 @@ public class Generator {
                 int choiceVerticalPos = defaultVerticalPos;
                 for(int l = 0; l < choicesCount; l++) {
                     code += generateCode(lines[i + l].substring(3), choiceColors[l], lineBreakCounts[i + l], i + l, choiceVerticalPos);
-                    int newlines = Math.max(1, lineBreakCounts[i + l]);
+                    int newlines = Math.max(1, lineBreakCounts[i + l] - 1);
                     choiceVerticalPos += (newlines * lineHeight);
                 }
 
